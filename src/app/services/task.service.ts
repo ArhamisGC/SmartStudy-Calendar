ac60fd95f08c08da24b40805d7cf4d7889ce4f37
m4rc <marcos.sanchez2003@proton.me>
2024-04-12 11:39:12 +0100

    Asignaciónn automática de la variable order

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import Task from '../interfaces/task.interface';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
-import { Auth } from '@angular/fire/auth'; // Asegúrate de importar Auth
+import { Auth } from '@angular/fire/auth';
+import {take} from "rxjs";
+import {map, switchMap} from "rxjs/operators"; // Asegúrate de importar Auth
 
 @Injectable({
   providedIn: 'root',
@@ -15,17 +17,39 @@ export class TaskService {
 
   getTasks() {
     const userId = this.getUserId();
-    return this.firestore.collection<Task>(`users/${userId}/tasks`).valueChanges({ idField: 'id' });
+    return this.firestore.collection<Task>(`users/${userId}/tasks`, ref => ref.orderBy('order')).valueChanges({ idField: 'id' });
   }
 
   addTask(task: Omit<Task, 'id'>) {
     const userId = this.getUserId();
-    this.firestore.collection<Task>(`users/${userId}/tasks`).add(task as Task);
+    this.firestore.collection<Task>(`users/${userId}/tasks`)
+      .valueChanges()
+      .pipe(
+        take(1),
+        map(tasks => tasks.length > 0 ? Math.max(...tasks.map(t => t.order)) + 1 : 1),
+        switchMap(order => {
+          task.order = order;
+          return this.firestore.collection<Task>(`users/${userId}/tasks`).add(task as Task);
+        })
+      ).subscribe();
   }
 
   removeTask(taskId: string) {
     const userId = this.getUserId();
-    this.firestore.collection<Task>(`users/${userId}/tasks`).doc(taskId).delete();
+    this.firestore.collection<Task>(`users/${userId}/tasks`).doc(taskId).delete().then(() => {
+      this.adjustOrdersAfterDeletion(userId);
+    });
+  }
+
+  private adjustOrdersAfterDeletion(userId: string) {
+    this.firestore.collection<Task>(`users/${userId}/tasks`, ref => ref.orderBy('order'))
+      .snapshotChanges()
+      .pipe(take(1))
+      .subscribe(snapshot => {
+        snapshot.forEach((doc, index) => {
+          doc.payload.doc.ref.update({ order: index + 1 });
+        });
+      });
   }
 
   updateTask(id: string, changes: Partial<Task>) {
