dbf23bed12d31e1499b5c3c0b3766d8a6be260e9
alvarotrancho <alvaro.virgo.99@gmail.com>
2024-05-10 11:26:16 +0100

    User crud implemented, user's can now modify their name, telephone, universiry and upload a profile picture. Dark mode implemented in profile.

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,9 +1,8 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
 import {UserService} from "../../services/user.service";
 import {User} from "../../interfaces/user.interface";
-import {Router} from "@angular/router";
 import {animate, style, transition, trigger} from "@angular/animations";
-
+import {DOCUMENT} from "@angular/common";
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
@@ -22,28 +21,86 @@ import {animate, style, transition, trigger} from "@angular/animations";
 })
 
 export class ProfileComponent implements OnInit {
-  userData: User | null = null;
-  selectedButton: string = 'Perfil';
-  isListVisible: boolean = true;
+  userData: User = {
+    profilePicture: 'https://via.placeholder.com/250',
+    nombreCompleto: '',
+    email: '',
+    telefono: '',
+    universidad: '',
+    fechaNacimiento: new Date,
+    password: '',
+    confirmPassword: ''
+  };
+  isEditing = false;
+  tempUser!:User;
 
-  constructor(private router: Router,private userService: UserService) {}
 
-  userEmail: string | null | undefined;
+  constructor(@Inject(DOCUMENT) private document: Document,private userService: UserService) {}
+
   ngOnInit(): void {
-    // @ts-ignore
-    this.userService.getUserData().subscribe(userData => {
+    this.userService.getUserData().subscribe((userData: User) => {
       if (userData) {
         this.userData = userData;
+        this.tempUser = {...userData};
       }
     });
   }
-  toggleListVisibility(button: string) {
-    this.selectedButton = button;
-    this.isListVisible = button === 'Perfil';
+
+  editProfile() {
+    this.isEditing = true;
+    this.tempUser = {...this.userData};
   }
 
-  Logout() {
-    this.userService.logout();
-    this.router.navigate(['/home']);
+  saveProfile() {
+    if (this.tempUser) {
+      const updatedUserData: Partial<User> = {};
+
+      if (this.tempUser.profilePicture !== undefined) {
+        updatedUserData.profilePicture = this.tempUser.profilePicture;
+      }
+      if (this.tempUser.universidad !== undefined) {
+        updatedUserData.universidad = this.tempUser.universidad;
+      }
+      if (this.tempUser.telefono !== undefined) {
+        updatedUserData.telefono = this.tempUser.telefono;
+      }
+      if (this.tempUser.nombreCompleto !== undefined) {
+        updatedUserData.nombreCompleto = this.tempUser.nombreCompleto;
+      }
+
+      this.userService.updateUserData(this.userService.getUID(), updatedUserData)
+        .then(() => {
+          this.userData = {...this.userData, ...updatedUserData};
+          this.isEditing = false;
+        })
+        .catch((error) => {
+          console.error('Error updating user data: ', error);
+        });
+    }
   }
+
+  onFileSelected(event: any) {
+    const file = event.target.files[0];
+    if (file) {
+      this.userService.uploadProfilePicture(file)
+        .then((downloadURL) => {
+          this.tempUser.profilePicture = downloadURL;
+          this.userService.updateUserData(this.userService.getUID(), {profilePicture: downloadURL})
+            .then(() => {
+              this.userData.profilePicture = downloadURL;
+            })
+            .catch((error) => {
+              console.error('Error updating profile picture URL in Firebase:', error);
+            });
+        })
+        .catch((error) => {
+          console.error('Error uploading profile picture:', error);
+        });
+    }
+  }
+
+  openFileInput() {
+    this.document.getElementById('file')?.click();
+  }
+
 }
